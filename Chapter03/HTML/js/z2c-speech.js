ba949e9df26ebd8b4cacf7a572b10cde32fd943c
emb2017 <mesdocuments@MacBook-Pro-de-Eric.local>
2017-05-14 12:08:35 +0200

    essai

--- Chapter03/HTML/js/z2c-speech.js
+++ Chapter03/HTML/js/z2c-speech.js
@@ -19,5 +19,40 @@
 
 function initPage ()
 {
+* Enable the microphone, disable the stop button:
+      var _mic = $('#microphone'); var _stop = $("#stop");
+    _mic.addClass("mic_enabled");
+    _stop.addClass("mic_disabled");
+
+* Monitor the microphone button for a click:
+  _mic.on("click", function ()
+    {
+      var _className = this.className;
+      if(this.className == "mic_enabled")
+      {
+        _mic.addClass("mic_disabled");
+        _mic.removeClass("mic_enabled");
+        _stop.addClass("mic_enabled");
+        _stop.removeClass("mic_disabled");
+        $.when($.get('/api/speech-to-text/token')).done(
+          function (token) {
+            stream = WatsonSpeech.SpeechToText.recognizeMicrophone({
+               token: token,
+               outputElement: '#speech' // CSS selector or DOM Element
+             });
+            stream.on('error', function(err) { console.log(err); });
+          });
+        }
+      });
+      
+* Monitor the Stop button for a click:
+  _stop.on("click",  function() {
+    console.log("Stopping text-to-speech service...");
+    if (stream != undefined) {stream.stop(); }
+    _mic.addClass("mic_enabled");
+    _mic.removeClass("mic_disabled");
+    _stop.addClass("mic_disabled");
+    _stop.removeClass("mic_enabled");
+});
 
 }
